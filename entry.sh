7d324c2d8d0119c8aaa3a6304b572b63729e8370
mahmoodfathy <71036818+mahmoodfathy@users.noreply.github.com>
2021-01-30 16:42:14 +0200

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -13,6 +13,7 @@ echo "  ____            _       ____  ____  __  __ ____
  | |_) | (_| \__ \ | | | | |_| | |_) | |  | |___) |
  |____/ \__,_|___/_| |_| |____/|____/|_|  |_|____/ "
 
+echo 
 options=("Press 1 to create database" "press 2 to list databases" "press 3 to connect to database" "press 4 to drop database" "press 5 to exit")
 select choice in "${options[@]}"
 do
